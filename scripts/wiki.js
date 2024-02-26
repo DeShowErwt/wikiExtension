f2b902422a012ac059bcd693aeaee64e6bd9294d
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-02-26 23:30:38 +0100

    Base API call exists, comment on how to continue

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -1,11 +1,19 @@
 // Use this file to make calls to the Wikipedia API
-var outlet = document.querySelector('#search_outlet');
-var header = document.getElementById('help');
-var errorContent = document.querySelector('error-content');
+const outlet = document.querySelector('#search_outlet');
+const header = document.getElementById('help');
+const errorContent = document.querySelector('error-content');
+let wikiURL = "https://en.wikipedia.org/w/api.php?"
 
-function run(){
-    errorContent.classList.remove('hidden');
-    console.log("i ran")
+// Make this listen for any messages and execute this on that message
+async function getWikiPage(page){
+    wikiURL += new URLSearchParams({
+        origin: "*",
+        action: "parse",
+        page: page.replace(' ', '_'),
+        format: "json"
+    })
+    
+    const req = await fetch(wikiURL)
+    const json = await req.json();
+    outlet.textContent = json
 }
-
-// Anything outside of a function is ran when the button is clicked
