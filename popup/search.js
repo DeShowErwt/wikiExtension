305aeaa5c175d4686798dad7da3a89eec06f595c
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-02-28 15:09:51 +0100

    Clarifying comments

--- popup/search.js
+++ popup/search.js
@@ -7,12 +7,16 @@ const searchInput = document.querySelector('#query')
 
 function listenForClick(){
     searchButton.addEventListener("click", (e) => {
-        //Working function
         let query = searchInput.value
-        // if(query.trim()===''){reportExecuteScriptError('No input detected');return}
+        // Only search if an actual query has been entered
+        if(query.trim()===''){reportExecuteScriptError('No input detected');return}
+
         console.log(searchInput.value)
+        // We are handling a url so spaces are not allowed, to make the input easier for the user we allow them to fill in spaces and convert those to _
         query = query.replace(' ', '_')
+        // The base of any api call
         let wikiURL = "https://en.wikipedia.org/w/api.php?"
+        // We only want to search on the parse API as that provides us with the page html which we need to converse
         wikiURL += new URLSearchParams({
             origin: "*",
             action: "parse",
@@ -28,16 +32,24 @@ function listenForClick(){
                 // Use JavaScript DOM manipulation to remove html tags, also allows for getting or removing specific elements
                 const tempElement = document.createElement('div')
                 tempElement.innerHTML=wiki_html
+                // Only get the text from the just entered html
                 const text = tempElement.textContent 
+                // Only display the first n chars, we dont want to place the whole page within the small extension box but mainly want to display the introduction part
                 outlet.textContent = text.substring(0,800);
+                // If the user wants to read more we can insert the wikipedia url in this link easily and place it under the output
                 page_link.innerHTML = '<a href="https://en.wikipedia.org/wiki/' + query + '" class="accent">Continue reading...</a>'
             })  
     })
 }
 
+// Run if an error is detected to display the error text
 function reportExecuteScriptError(error){
     content.classList.add("hidden")
     error_content.classList.remove('hidden');
 }
 
-listenForClick()
+try{
+    listenForClick()
+}
+//If anything goes wrong we send that error to the reportExecuteScriptError
+catch(e){reportExecuteScriptError(e)}
